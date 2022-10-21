e90ca31b78bbb713e12a4738da9fe24024e07648
Pallavi Varshney <pallavivarshney6@gmail.com>
2022-10-21 10:58:19 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        email: "abcdef123@gmail.com",
+        password: "9876543210"
+    }
+]
+
+const publicPosts = [
+    {
+        title: "Free Tips on Development",
+        content: "These are some tips"
+    }
+]
+
+const privatePosts = [
+    {
+        title: "Paid Tips on Development",
+        content: "These are some tips"
+    }
+]
+
+module.exports = {
+    users,
+    publicPosts,
+    privatePosts
+}
